1de43e8c0ccf1b8ebff8ff00c6a6a5e890d447e9
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-05-03 11:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+
+let words= document.getElementById('wordCount');
+function count(text){
+let text=document.getElementById('evaluatedText').value;
+let  spaces=text.match(/\S+/g);
+	let words=spaces? spaces.length+1:0;
+	let wordCount=document.getElementById('wordCount');
+	wordCount.innerText=words;
+	
+	
+	
+	
+}
