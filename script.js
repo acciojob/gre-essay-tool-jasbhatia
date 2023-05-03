11d2f3e2b90dd00550ddb3401319881cdbc565ed
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-05-03 11:32:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //your code here
 
-let words= document.getElementById('wordCount');
-function count(text){
-let text=document.getElementById('evaluatedText').value;
-let  spaces=text.match(/\S+/g);
-	let words=spaces? spaces.length+1:0;
-	let wordCount=document.getElementById('wordCount');
-	wordCount.innerText=words;
+// let words= document.getElementById('wordCount');
+function count(self) {
+  let text = document.getElementById("evaluatedText").value;
+  let spaces = self.value.match(/\S+/g);
+  let words = spaces ? spaces.length : 0;
+  // console.log(words);
+  let wordCount = document.getElementById("wordCount");
+  // console.log(wordCount);
+  wordCount.innerText = words;
+}
 	
 	
 	
