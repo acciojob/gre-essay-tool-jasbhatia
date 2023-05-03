dac43677ce245fb70c1d09cb5231dcfea4ca56b7
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-05-03 12:38:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
 
 // let words= document.getElementById('wordCount');
 function count(self) {
-  let text = document.getElementById("evaluatedText").value;
-  let spaces = self.value.match(/\S+/g);
-  let words = spaces ? spaces.length : 0;
-  // console.log(words);
-  let wordCount = document.getElementById("wordCount");
-  // console.log(wordCount);
-  wordCount.innerText = words;
-}
+  // let text = document.getElementById("evaluatedText").value.trim();
+  // let spaces = self.value.match(/\S+/g);
+  // let words = spaces ? spaces.length : 0;
+  // // console.log(words);
+  // let wordCount = document.getElementById("wordCount");
+  // // console.log(wordCount);
+  // wordCount.innerText = words;
+
+	let text = document.getElementById("evaluatedText").value.trim();
+			let wordCount = text.split(/\s+/).length;
+			document.getElementById("wordCount").innerHTML = wordCount;
+// }
 	
 	
 	
